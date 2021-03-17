9a130f7eb3eb22269b196b3d4399c0323439ca26
Eclixal <alex.jousset1@gmail.com>
2021-03-17 01:47:05 +0100

    configure trevis for build

--- kairos/src/main/java/fr/eclixal/Kairos.java
+++ kairos/src/main/java/fr/eclixal/Kairos.java
@@ -2,4 +2,8 @@ package fr.eclixal;
 
 public class Kairos {
 
+    public int add(int a, int b) {
+        return a + b;
+    }
+
 }
--- kairos/src/test/java/fr/eclixal/TestKairos.java
+++ kairos/src/test/java/fr/eclixal/TestKairos.java
@@ -1,5 +1,16 @@
 package fr.eclixal;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestKairos {
 
+    @Test
+    void testAdd() {
+        Kairos kairos = new Kairos();
+        assertEquals(3, kairos.add(1,2));
+    }
+
 }
+
